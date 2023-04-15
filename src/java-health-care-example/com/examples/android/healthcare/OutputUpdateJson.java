98f053436bf894d01cf1a10ed3e1da938c278f25
yoshida.yukio <pipito.yukio@gmail.com>
2023-04-15 19:38:25 +0900

    Update OutputUpdateJson.java
    Use JsonTemplate.java class

--- src/java-health-care-example/com/examples/android/healthcare/OutputUpdateJson.java
+++ src/java-health-care-example/com/examples/android/healthcare/OutputUpdateJson.java
@@ -1,5 +1,6 @@
 package com.examples.android.healthcare;
 
+import com.examples.android.healthcare.constants.JsonTemplate;
 import com.examples.android.healthcare.data.*;
 import com.examples.android.healthcare.util.FileUtil;
 import com.google.gson.Gson;
@@ -18,16 +19,6 @@ import java.util.ArrayList;
  */
 public class OutputUpdateJson {
     static final String JSON_NAME = "update_data.json";
-    // 部分更新のためのテンプレート
-    private static final String TEMPL_MAIN = "{\"emailAddress\":\"%s\",\"measurementDay\":\"%s\",\"healthcareData\":{%s}," +
-            "\"weatherData\":{%s}}";
-    // テストとして睡眠管理データと歩数データ、天候データのみを更新する
-    private static final String TEMPL_HEALTH_SM = "\"sleepManagement\":%s";
-    private static final String TEMPL_HEALTH_BP = "\"bloodPressure\":%s";
-    private static final String TEMPL_HEALTH_BT = "\"bodyTemperature\":%s";
-    private static final String TEMPL_HEALTH_NF = "\"nocturiaFactors\":%s";
-    private static final String TEMPL_HEALTH_WC = "\"walkingCount\":%s";
-    private static final String TEMPL_WEATHER = "\"weatherCondition\": %s";
 
     public static <List> void main(String[] args) {
         // 出力ファイル名
@@ -37,52 +28,43 @@ public class OutputUpdateJson {
         Gson gson = new GsonBuilder().serializeNulls().create();
 
         // Androidアプリで使用する前提で実装
+        String jsonContent;
+        String JsonWithProperty;
         // 健康管理データ
         java.util.List<String> healthList = new ArrayList<String>();
         // 1.睡眠管理データ: sleepScoreを変更
         SleepManagement sleepManagement = new SleepManagement(
                 "05:30", 70, "06:40", "0:50"
         );
-        String sleepMan = gson.toJson(sleepManagement);
-        String jsonSleep = String.format(TEMPL_HEALTH_SM, sleepMan);
-        healthList.add(jsonSleep);
-        // 2.血圧測定データ
-//        BloodPressure bloodPressure = new BloodPressure(
-//                "06:50", 124, 76, 73,
-//                "22:15", 137, 80, 57
-//        );
-//        String bloodPress = gson.toJson(bloodPressure);
-//        String jsonBlood = String.format(TEMPL_HEALTH_BP, bloodPress);
-//        healthList.add(jsonBlood);
-        // 4.夜間頻尿要因
-//        NocturiaFactors factors = new NocturiaFactors(
-//                1,true, false, false, false, false,
-//                false,false, true, "今日は疲れた"
-//        );
-//        String noctFactors = gson.toJson(factors);
-//        String jsonFactors = String.format(TEMPL_HEALTH_NF, noctFactors);
-//        healthList.add(jsonFactors);
+        jsonContent = gson.toJson(sleepManagement);
+        JsonWithProperty = JsonTemplate.getJsonWithSleepManagement(jsonContent);
+        healthList.add(JsonWithProperty);
+        // 2.血圧測定データ: 変更なし
+        // 3.体温測定データ: 変更なし
+        // 4.夜間頻尿要因: 変更なし
         // 5.歩数: 変更
         WalkingCount walkingCount = new WalkingCount(8300);
-        String wc = gson.toJson(walkingCount);
-        String jsonWc = String.format(TEMPL_HEALTH_WC, wc);
-        healthList.add(jsonWc);
+        jsonContent = gson.toJson(walkingCount);
+        JsonWithProperty = JsonTemplate.getJsonWithWalkingCount(jsonContent);
+        healthList.add(JsonWithProperty);
         // 健康管理データ
-        String healthcareData = String.join(",", healthList);
-        System.out.println(healthcareData);
+        String healthcareDataJson = String.join(",", healthList);
+        System.out.println(healthcareDataJson);
 
         // 天気情報
         WeatherCondition weatherCondition = new WeatherCondition("晴れ時々曇り");
-        String weather = gson.toJson(weatherCondition);
-        String jsonWeather = String.format(TEMPL_WEATHER, weather);
-        System.out.println(jsonWeather);
+        jsonContent = gson.toJson(weatherCondition);
+        String weatherDataJson = JsonTemplate.getJsonWithWeatherCondition(jsonContent);
+        System.out.println(weatherDataJson);
 
         // メールアドレスと測定日が各テーブルの主キー
         String emailAddress = "user1@examples.com";
         String measurementDay = "2023-03-10";
-        String json = String.format(TEMPL_MAIN, emailAddress, measurementDay, healthcareData, jsonWeather);
+        // 更新用リクエスト用のJSON文字列をテンプレートから生成
+        String updateJson = JsonTemplate.createUpdateJson(emailAddress, measurementDay,
+                healthcareDataJson, weatherDataJson);
         try {
-            FileUtil.saveText(saveFile, json);
+            FileUtil.saveText(saveFile, updateJson);
         } catch (IOException e) {
             System.out.println(e);
         }
