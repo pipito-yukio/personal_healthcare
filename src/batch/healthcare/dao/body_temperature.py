4b9924e06019357d78bd5d9e099910bd940e0bf3
yoshida.yukio <pipito.yukio@gmail.com>
2023-04-19 19:39:51 +0900

    Update body_temperature.py
    Add field

--- src/batch/healthcare/dao/body_temperature.py
+++ src/batch/healthcare/dao/body_temperature.py
@@ -1,11 +1,10 @@
-from datetime import date
+from datetime import date, time
 from sqlalchemy import Float
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from dao import HealthcareBase
 
-# 健康管理データベース
 # 体温管理テーブルデータクラス
 
 
@@ -15,9 +14,11 @@ class BodyTemperature(HealthcareBase):
     # pid: Mapped[SmallInteger] = mapped_column(ForeignKey("person.id"), name='pid',  primary_key=True)
     pid: Mapped[int] = mapped_column(name='pid', primary_key=True)
     measurementDay: Mapped[date] = mapped_column(name='measurement_day', primary_key=True)
+    measurementTime: Mapped[time] = mapped_column(name='measurement_time')
     temperature: Mapped[float] = mapped_column(Float, name='temperature')
 
     def __repr__(self):
         return f"BodyTemperature(pid={self.pid!r}, " \
                f"measurementDay={self.measurementDay!r}, " \
+               f"measurementTime={self.measurementTime!r}," \
                f"temperature={self.temperature!r})"
