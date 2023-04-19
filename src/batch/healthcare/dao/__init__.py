eeef8609ff6c7bdc0b73b419a182a2a910a0fa52
yoshida.yukio <pipito.yukio@gmail.com>
2023-04-19 19:41:24 +0900

    Update __init__.py
    Change schema name

--- src/batch/healthcare/dao/__init__.py
+++ src/batch/healthcare/dao/__init__.py
@@ -2,5 +2,5 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
 
 # 健康管理データベース共通: PostgreSQL schema定義
-healthcare_schema = MetaData(schema="bodyhealth")
-HealthcareBase = declarative_base(metadata=healthcare_schema)
+schema = MetaData(schema="bodyhealth")
+HealthcareBase = declarative_base(metadata=schema)
