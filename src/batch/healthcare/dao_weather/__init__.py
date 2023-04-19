da1f0b3dbe2d1b91f945e1c20de6bd5afc5b88fd
yoshida.yukio <pipito.yukio@gmail.com>
2023-04-19 19:41:01 +0900

    Delete __init__.py

--- src/batch/healthcare/dao_weather/__init__.py
+++
@@ -1,6 +0,0 @@
-# from sqlalchemy import MetaData
-# from sqlalchemy.orm import declarative_base
-#
-# # センサーデータベース
-# weather_schema = MetaData(schema="weather")
-# WeatherBase = declarative_base(metaclass=weather_schema)
