b306af5405db995993db0c4ee663532d40e41e4c
yoshida.yukio <pipito.yukio@gmail.com>
2023-04-26 19:48:17 +0900

    bug fix
    JSON property name

--- src/batch/healthcare/GetHealthcareDataWithSelector.py
+++ src/batch/healthcare/GetHealthcareDataWithSelector.py
@@ -15,6 +15,8 @@ DB_HEALTHCARE_CONF: str = os.path.join("conf", "db_healthcare.json")
 DB_SENSORS_CONF: str = os.path.join("conf", "db_sensors.json")
 SAVE_JSON = os.path.join("json_datas", 'out_healthcare_data.json')
 LOG_FMT = '%(asctime)s %(filename)s %(funcName)s %(levelname)s %(message)s'
+# デバックログ有効
+app_logger_debug: bool = True
 
 
 def save_text(filePath: str, text) -> None:
@@ -33,7 +35,7 @@ def get_conn_dict(filePath: str) -> dict:
 
 if __name__ == '__main__':
     logging.basicConfig(format=LOG_FMT, level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
+    app_logger = logging.getLogger(__name__)
 
     # 健康管理データベース: postgresql[5433]
     conn_dict: dict = get_conn_dict(DB_HEALTHCARE_CONF)
@@ -42,7 +44,7 @@ if __name__ == '__main__':
     Cls_sess_healthcare: scoped_session = scoped_session(
         sessionmaker(bind=engine_healthcare)
     )
-    logger.info(f"Cls_sess_healthcare: {Cls_sess_healthcare}")
+    app_logger.info(f"Cls_sess_healthcare: {Cls_sess_healthcare}")
 
     # 2.気象センサデータベース
     # 気象センサーデータベース: postgresql[5432]
@@ -52,25 +54,30 @@ if __name__ == '__main__':
     Cls_sess_sensors: scoped_session = scoped_session(
         sessionmaker(bind=engine_sensors)
     )
-    logger.info(f"Cls_sess_sensors: {Cls_sess_sensors}")
+    app_logger.info(f"Cls_sess_sensors: {Cls_sess_sensors}")
 
     emailAddress: str = "user1@examples.com"
     measurementDay: str = "2023-03-14"
-    selector = Selector(Cls_sess_healthcare, Cls_sess_sensors, logger=logger)
+    selector = Selector(Cls_sess_healthcare, Cls_sess_sensors, logger=app_logger)
     healthcare_dict: Dict = selector.get_healthcare_asdict(emailAddress, measurementDay)
     if healthcare_dict:
-        logger.info(healthcare_dict)
+        app_logger.info(healthcare_dict)
 
         weather_dict = selector.get_weather_asdict(measurementDay)
         if weather_dict:
-            logger.info(weather_dict)
+            app_logger.info(weather_dict)
 
             healthcare_dict["emailAddress"] = emailAddress
             healthcare_dict["measurementDay"] = measurementDay
-            healthcare_dict["weatherCondition"] = weather_dict
-            # https://stackoverflow.com/questions/64560044/reading-template-json-file-and-creating-new-file-with-substitutions-in-bash-or-p
-            #  reading template JSON file and creating new file with substitutions in bash or Python
-            logger.debug(weather_dict)
+            # 天気状態取得
+            weather_dict = selector.get_weather_asdict(measurementDay)
+            if app_logger_debug:
+                app_logger.debug(f"Weather: {weather_dict}")
+            if weather_dict:
+                healthcare_dict["weatherData"] = weather_dict
+            else:
+                # 天候がなければ未設定
+                healthcare_dict["weatherData"] = None
             # 日本語が含まれる: ensure_ascii=False
             json_str = json.dumps(healthcare_dict, indent=3, ensure_ascii=False)
             save_text(SAVE_JSON, json_str)
