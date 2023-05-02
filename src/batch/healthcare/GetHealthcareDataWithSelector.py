4aaf8e86697fe62be43d2c984389adf86c75c0b6
yoshida.yukio <pipito.yukio@gmail.com>
2023-05-02 19:10:54 +0900

    Generate JSON text the same as the web application response.

--- src/batch/healthcare/GetHealthcareDataWithSelector.py
+++ src/batch/healthcare/GetHealthcareDataWithSelector.py
@@ -90,19 +90,21 @@ if __name__ == '__main__':
         app_logger.info(healthcare_dict)
 
         # 2-2. 検索オブジェクトの天候状態データ取得関数を呼び出す
-        weather_dict = selector.get_weather_asdict(measurementDay)
+        weather_dict: Dict = selector.get_weather_asdict(measurementDay)
         if weather_dict:
             # 天候データは必ずある想定
             app_logger.info(weather_dict)
             healthcare_dict["weatherData"] = weather_dict
 
             # 3. 辞書オブジェクトを登録データ取得レスポンス形式(JSON)でファイル保存
-            # ※ Flaskアプリでのレスポンスの生成を模倣
             healthcare_dict["emailAddress"] = emailAddress
             healthcare_dict["measurementDay"] = measurementDay
+            # ※ Flaskアプリでのレスポンスの生成を模倣
+            result_dict: Dict = {"data": healthcare_dict,
+                                 "status": {"code": 0, "message": "OK"}}
             # 日本語が含まれるため: ensure_ascii=False
             # 3-1. 辞書オブジェクトをJSON形式文字列に変換
-            json_str = json.dumps(healthcare_dict, indent=3, ensure_ascii=False)
+            json_str = json.dumps(result_dict, indent=3, ensure_ascii=False)
             # 3-2. JSON文字列をファイル保存
             # ※保存されるJSONはFlaskアプリのレスポンスと同一となる
             save_text(saveJsonFile, json_str)
