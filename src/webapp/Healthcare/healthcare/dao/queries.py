4236b36a4fac24166104904470ba37f5d00d5b8d
yoshida.yukio <pipito.yukio@gmail.com>
2023-05-03 18:57:38 +0900

    Delete unnecessary comments

--- src/webapp/Healthcare/healthcare/dao/queries.py
+++ src/webapp/Healthcare/healthcare/dao/queries.py
@@ -129,11 +129,6 @@ SELECT condition FROM weather.weather_condition WHERE measurement_day=:measureme
         :return: 健康管理データの辞書オブジェクト, 存在しない場合はNone
         """
         # パラメータ辞書生成: 主キー
-        # https://docs.sqlalchemy.org/en/20/orm/session_transaction.html
-        # will automatically begin again
-        # result = session.execute("< some select statement >")
-        # session.add_all([more_objects, ...])
-        # session.commit()  
         params = {"emailAddress": email, "measurementDay": measurement}
         try:
             rs: Result = self.sess_healthcare.execute(text(self._QRY_GET_HEALTHCARE), params)
